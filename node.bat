b9a3ed1d26e0dc65970430d6c9600292917e6589
Opticulex <authorfilmsstudios@gmail.com>
2017-06-16 19:38:45 +0100

    Add files via upload

---
+++ node.bat
@@ -0,0 +1,3 @@
+set PATH = %PATH%; %cd%\
+set NODE_PATH=%cd%\package\node_modules
+node update.js
---
+++ update.js
@@ -0,0 +1,12 @@
+var NeoCities = require('neocities')
+var api = new NeoCities('YOURUSERNAME', 'YOURPASSWORD')
+
+Uploading files to your site
+
+// local file path is ./index.js, saved on site as derp.js
+
+api.upload([
+  {name: 'derp.js', path: './index.js'}
+], function(resp) {
+  console.log(resp)
+}))
